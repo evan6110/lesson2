4f1f04f7ab83b4155b86d3c5f77dd69133d785f2
evan6110 <evan6110@ISS147D8-09.scdsb.pri>
2015-09-11 10:35:46 -0400

    Pizza Dog

--- src/PizzaCost.java
+++ src/PizzaCost.java
@@ -2,11 +2,13 @@ import hsa.*;
 
 public class PizzaCost {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Console c = new Console();
+        int size, topam;
+        double lab = 1.5, util = 0.75, topco = 0.75, base = 0.5;
+        c.println("==================Pizza Cost==================");
+        c.print("Enter the size of the pizza(inches): ");
     }
     
 }
